81216ffc4e86b7414dc78e6290bedd44193e8a7b
Michael Bathon <michaelbathon@Zips-MacBook-Pro-2.local>
2016-01-28 12:20:57 -0500

    Completed InitLab1 Part1 and Test

--- src/main/java/bathon/michael/initlab01/InitializationLab1.java
+++ src/main/java/bathon/michael/initlab01/InitializationLab1.java
@@ -5,6 +5,47 @@ package bathon.michael.initlab01;
  */
 public class InitializationLab1 {
 
+    public static void main(String[] args) {
+
+     ColorfulThing ctRed = new ColorfulThing("red");
+        ColorfulThing ctBlue = new ColorfulThing("blue");
+        ColorfulThing ctGreen = new ColorfulThing("green");
+        ColorfulThing ctYellow = new ColorfulThing("yellow");
+        ColorfulThing ctOrange = new ColorfulThing("orange");
+       // ColorfulThing purple = new ColorfulThing(); test no arg constructor causes error
+
+        BoringThing boring1 = new BoringThing();
+        BoringThing boring2 = new BoringThing();
+        BoringThing boring3 = new BoringThing();
+        BoringThing boring4 = new BoringThing();
+        BoringThing boring5 = new BoringThing();
+
+        System.out.println(ctRed.getColor());
+        System.out.println(ctBlue.getColor());
+        System.out.println(ctGreen.getColor());
+        System.out.println(ctYellow.getColor());
+        System.out.println(ctOrange.getColor());
+
+
+    }
+}
+
+class ColorfulThing{
+
+    private String color; //variable for first constructor
+
+    ColorfulThing(String color) {
+        this.color = color;
+    } //Constructor for part 1 of Lab
+
+   public String getColor(){
+        return this.color;
+    } //Getter method for part 1 of Lab
+
+}
+
+class BoringThing{
 
 
 }
+
--- src/test/java/bathon/michael/initlab01/SpecInitializationLab1.java
+++ src/test/java/bathon/michael/initlab01/SpecInitializationLab1.java
@@ -1,9 +1,25 @@
 package bathon.michael.initlab01;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by michaelbathon on 1/28/16.
  */
 public class SpecInitializationLab1 {
+
+    ColorfulThing testColor;
+
+    @Before
+    public void setUp(){testColor = new ColorfulThing("blue");}
+
+    String colorExpected = "blue";
+
+    @Test
+    public void testGetColor(){
+        assertEquals(colorExpected, testColor.getColor());
+    }
 
 
 }
