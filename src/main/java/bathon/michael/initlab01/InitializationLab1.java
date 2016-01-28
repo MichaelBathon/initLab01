96174014e4215c51f96a9b212e1e104c237f0445
Michael Bathon <michaelbathon@Zips-MacBook-Pro-2.local>
2016-01-28 10:20:19 -0500

    first commit

---
+++ src/main/java/bathon/michael/initlab01/InitializationLab1.java
@@ -0,0 +1,10 @@
+package bathon.michael.initlab01;
+
+/**
+ * Created by michaelbathon on 1/27/16.
+ */
+public class InitializationLab1 {
+
+
+
+}
---
+++ src/test/java/bathon/michael/initlab01/SpecInitializationLab1.java
@@ -0,0 +1,9 @@
+package bathon.michael.initlab01;
+
+/**
+ * Created by michaelbathon on 1/28/16.
+ */
+public class SpecInitializationLab1 {
+
+
+}
